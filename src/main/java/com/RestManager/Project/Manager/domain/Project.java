15f331919814aef0b14cde4546d3906d5b968dc2
MikhaelPrice <90347499+MikhaelPrice@users.noreply.github.com>
2022-07-27 13:22:35 +0300

    Added repo interface

---
+++ src/main/java/com/RestManager/Project/Manager/domain/Project.java
@@ -0,0 +1,48 @@
+package com.RestManager.Project.Manager.domain;
+
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Entity
+@Table
+@ToString(of = {"id", "text"})
+public class Project {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Project project = (Project) o;
+        return id != null && Objects.equals(id, project.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
+
