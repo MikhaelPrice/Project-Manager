671850438600c1b003c976e994e0c4415044351e
MikhaelPrice <90347499+MikhaelPrice@users.noreply.github.com>
2022-07-28 14:37:38 +0300

    Added WebPack

--- src/main/java/com/RestManager/Project/Manager/controller/MainController.java
+++ src/main/java/com/RestManager/Project/Manager/controller/MainController.java
@@ -3,6 +3,7 @@ package com.RestManager.Project.Manager.controller;
 import com.RestManager.Project.Manager.domain.User;
 import com.RestManager.Project.Manager.repo.ProjectRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,9 @@ import java.util.HashMap;
 @RequestMapping("/")
 public class MainController {
 
+    @Value("${spring.profiles.active}")
+    private String profile;
+
     @Autowired
     public MainController(ProjectRepo projectRepo) {
         this.projectRepo = projectRepo;
@@ -25,9 +29,13 @@ public class MainController {
     @GetMapping
     public String main(Model model, @AuthenticationPrincipal User user){
         HashMap<Object, Object> data = new HashMap<>();
+
         data.put("profile", user);
         data.put("projects", projectRepo.findAll());
+
         model.addAttribute("frontendData", data);
+        model.addAttribute("isDevMode","dev".equals(profile));
+
         return "index";
     }
 
